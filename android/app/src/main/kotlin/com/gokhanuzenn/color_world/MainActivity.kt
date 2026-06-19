e1352a10251a356bac6ad1edafae286d2b68daec
Gökhan ÜZEN <109869095+gokhanuzenn@users.noreply.github.com>
2026-06-19 22:35:41 +0300

    fix: simplify MainActivity.kt to standard v2 embedding to resolve build errors and potential registrant conflicts

--- android/app/src/main/kotlin/com/gokhanuzenn/color_world/MainActivity.kt
+++ android/app/src/main/kotlin/com/gokhanuzenn/color_world/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.gokhanuzenn.color_world
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
 }
