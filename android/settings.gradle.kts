325612506d507f44419a21236bf1bc8dfdf2d22e
Gökhan ÜZEN <109869095+gokhanuzenn@users.noreply.github.com>
2026-06-20 00:15:47 +0300

    fix: change RepositoriesMode to PREFER_PROJECT to resolve repository declaration conflict

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 include(":app")
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
