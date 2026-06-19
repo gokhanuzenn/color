8c0e585dc460064cfa36caeb12afd426e4a50caf
Gökhan ÜZEN <109869095+gokhanuzenn@users.noreply.github.com>
2026-06-19 21:48:56 +0300

    fix: inject namespace into subprojects to fix AGP 8 'Namespace not specified' errors for image_gallery_saver and other legacy plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,22 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        val project = this
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                android.namespace = project.group.toString()
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
